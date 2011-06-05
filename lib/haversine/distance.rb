a55161b52de6d327847b505decba8422e21c2864
Ryan Greenberg <ryangreenberg@gmail.com>
2011-06-05 12:53:34 -0700

    Refactor Haversine::Distance with convenience methods instead of hash access

---
+++ lib/haversine/distance.rb
@@ -0,0 +1,33 @@
+module Haversine
+  class Distance
+
+    GREAT_CIRCLE_RADIUS_MILES = 3956
+    GREAT_CIRCLE_RADIUS_KILOMETERS = 6371 # some algorithms use 6367
+    GREAT_CIRCLE_RADIUS_FEET = GREAT_CIRCLE_RADIUS_MILES * 5280
+    GREAT_CIRCLE_RADIUS_METERS = GREAT_CIRCLE_RADIUS_KILOMETERS * 1000
+
+    def initialize(great_circle_distance)
+      @great_circle_distance = great_circle_distance
+    end
+  
+    def to_miles
+      @great_circle_distance * GREAT_CIRCLE_RADIUS_MILES
+    end
+    alias_method :to_mi, :to_miles
+  
+    def to_kilometers
+      @great_circle_distance * GREAT_CIRCLE_RADIUS_KILOMETERS
+    end
+    alias_method :to_km, :to_kilometers
+  
+    def to_meters
+      @great_circle_distance * GREAT_CIRCLE_RADIUS_METERS
+    end
+    alias_method :to_m, :to_meters
+  
+    def to_feet
+      @great_circle_distance * GREAT_CIRCLE_RADIUS_FEET
+    end
+    alias_method :to_ft, :to_feet
+  end
+end
