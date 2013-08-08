1f263c28ba124ad83cfb2cc9a62e99e2b9e95ef6
Tim Krajcar <allegro@conmolto.org>
2013-08-08 15:23:21 -0700

    Add support for nautical miles.

--- lib/haversine/distance.rb
+++ lib/haversine/distance.rb
@@ -1,38 +1,44 @@
 module Haversine
   class Distance
     include Comparable
-    
+
     GREAT_CIRCLE_RADIUS_MILES = 3956
     GREAT_CIRCLE_RADIUS_KILOMETERS = 6371 # some algorithms use 6367
     GREAT_CIRCLE_RADIUS_FEET = GREAT_CIRCLE_RADIUS_MILES * 5280
     GREAT_CIRCLE_RADIUS_METERS = GREAT_CIRCLE_RADIUS_KILOMETERS * 1000
+    GREAT_CIRCLE_RADIUS_NAUTICAL_MILES = GREAT_CIRCLE_RADIUS_MILES / 1.15078
 
     attr_reader :great_circle_distance
 
     def initialize(great_circle_distance)
       @great_circle_distance = great_circle_distance
     end
-  
+
     def to_miles
       @great_circle_distance * GREAT_CIRCLE_RADIUS_MILES
     end
     alias_method :to_mi, :to_miles
-  
+
     def to_kilometers
       @great_circle_distance * GREAT_CIRCLE_RADIUS_KILOMETERS
     end
     alias_method :to_km, :to_kilometers
-  
+
     def to_meters
       @great_circle_distance * GREAT_CIRCLE_RADIUS_METERS
     end
     alias_method :to_m, :to_meters
-  
+
     def to_feet
       @great_circle_distance * GREAT_CIRCLE_RADIUS_FEET
     end
     alias_method :to_ft, :to_feet
-    
+
+    def to_nautical_miles
+      @great_circle_distance * GREAT_CIRCLE_RADIUS_NAUTICAL_MILES
+    end
+    alias_method :to_nm, :to_nautical_miles
+
     def <=>(other)
       great_circle_distance <=> other.great_circle_distance
     end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -2,7 +2,8 @@ $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
 $LOAD_PATH.unshift(File.dirname(__FILE__))
 require 'rspec'
 require 'haversine'
-
+require 'simplecov'
+SimpleCov.start if ENV["COVERAGE"]
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
 Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
