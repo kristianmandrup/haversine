62f003871da5d139d0b7f99f655d4a981aa12508
Kristian Mandrup <kmandrup@gmail.com>
2016-06-23 10:42:47 -0600

    MIT license

--- lib/haversine.rb
+++ lib/haversine.rb
@@ -1,28 +1,9 @@
 #
-# haversine formula to compute the great circle distance between two points given their latitude and longitudes
-#
-# Copyright (C) 2008, 360VL, Inc
-# Copyright (C) 2008, Landon Cox
-#
-# http://www.esawdust.com (Landon Cox)
-# contact:
-# http://www.esawdust.com/blog/businesscard/businesscard.html
-#
-# LICENSE: GNU Affero GPL v3
-# The ruby implementation of the Haversine formula is free software: you can redistribute it and/or modify
-# it under the terms of the GNU Affero General Public License version 3 as published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the
-# implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public
-# License version 3 for more details.  http://www.gnu.org/licenses/
-#
-# Landon Cox - 9/25/08
+# Haversine formula to compute the great circle distance between two points given their latitude and longitudes
 #
 # Notes:
 #
 # translated into Ruby based on information contained in:
-#   http://mathforum.org/library/drmath/view/51879.html  Doctors Rick and Peterson - 4/20/99
-#   http://www.movable-type.co.uk/scripts/latlong.html
 #   http://en.wikipedia.org/wiki/Haversine_formula
 #
 # This formula can compute accurate distances between two points given latitude and longitude, even for
